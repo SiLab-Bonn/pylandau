35101213ba50ecaef27cbe7516d99a413ba4c69a
DavidLP <david-leon.pohl@rub.de>
2015-08-25 19:12:02 +0200

    MAINT: preparing for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
-from setuptools import find_packages
 from distutils.extension import Extension
 from Cython.Build import cythonize
-from distutils.command.build_ext import build_ext
 import numpy as np
 
 extensions = [
     Extension('pyLandau.landau', ['pyLandau/cpp/landau.pyx'])
 ]
 
-version = '0.9.0'
+version = '1.0.0rc'
 author = 'David-Leon Pohl'
 author_email = 'pohl@physik.uni-bonn.de'
 
@@ -26,11 +24,11 @@ setup(
     author_email=author_email,
     maintainer_email=author_email,
     install_requires=['cython', 'numpy'],
-    packages=find_packages(),
     include_package_data=True,
+    packages=["pyLandau"],
     package_data={'': ['*.txt', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
     ext_modules=cythonize(extensions),
     include_dirs=[np.get_include()],
-    cmdclass={'build_ext': build_ext},
+    keywords=['Landau', 'Langau', 'PDF'],
     platforms='any'
 )
