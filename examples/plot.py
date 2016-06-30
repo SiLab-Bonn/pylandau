a83cf585ef2202f75fd1e6fb2f5d0d6431eb3222
DavidLP <david-leon.pohl@rub.de>
2016-06-30 18:12:15 +0200

    ENH: langau plot MAINT: new API

--- examples/plot.py
+++ examples/plot.py
@@ -5,13 +5,16 @@ import pylandau
 import matplotlib.pyplot as plt
 
 x = np.arange(0, 100, 0.01)
-
-for A, eta, mu in ((1, 1, 10), (1, 2, 30), (0.5, 5, 50)):
+for A, eta, mpv in ((1, 1, 10), (1, 2, 30), (0.5, 5, 50)):
     # Use the function that calculates y values when given array
-    plt.plot(x, pylandau.landau(x, mu, eta, A), '-', label='A=%1.1f, eta=%1.1f, mu=%1.1f' % (A, eta, mu))
+    plt.plot(x, pylandau.landau(x, mpv, eta, A), '-', label='mu=%1.1f, eta=%1.1f, A=%1.1f' % (mpv, eta, A))
 
     # Use the function that calculates the y value given a x value, (e.g. needed for minimizers)
-    y = np.array([pylandau.get_landau(x_value, mu, eta, A) for x_value in x])
-    plt.plot(x, y, '--', label='A=%1.1f, eta=%1.1f, mu=%1.1f' % (A, eta, mu))
+    y = np.array([pylandau.get_landau(x_value, mpv, eta, A) for x_value in x])
+    plt.plot(x, y, 'r--', label='mu=%1.1f, eta=%1.1f, A=%1.1f' % (mpv, eta, A))
+
+    # Use the function that calculates the y value given a x value, (e.g. needed for minimizers)
+    sigma = 10
+    plt.plot(x, pylandau.langau(x, mpv, eta, sigma, A), '--', label='mu=%1.1f, eta=%1.1f, sigma=%1.1f, A=%1.1f' % (A, eta, sigma, mpv))
 plt.legend(loc=0)
 plt.show()
