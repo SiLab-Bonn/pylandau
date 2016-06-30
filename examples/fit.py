71b241ef9d689abd2b0e35780bb88cc1f759dd66
DavidLP <david-leon.pohl@rub.de>
2016-06-30 13:03:51 +0200

    MAINT: use new package import

--- examples/fit.py
+++ examples/fit.py
@@ -1,13 +1,13 @@
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import numpy as np
-from pyLandau import landau
+import pylandau
 
 x = np.arange(0, 100, 0.5)
 mu, eta, sigma, A = 10, 1, 3, 1
-y = landau.langau(x, mu, eta, sigma, A) + np.random.normal(0, 0.05, 200)
+y = pylandau.langau(x, mu, eta, sigma, A) + np.random.normal(0, 0.05, 200)
 
-coeff, pcov = curve_fit(landau.langau, x, y, p0=(mu, eta, sigma, A))
+coeff, pcov = curve_fit(pylandau.langau, x, y, p0=(mu, eta, sigma, A))
 plt.plot(x, y, "o")
-plt.plot(x, landau.langau(x, *coeff), "-")
+plt.plot(x, pylandau.langau(x, *coeff), "-")
 plt.show()
--- examples/integrate.py
+++ examples/integrate.py
@@ -1,10 +1,10 @@
-from pyLandau import landau
+import pylandau
 # Check the integral of PDF ~ 1 with scipy numerical integration
 mu, eta, sigma, A = 10, 1, 3, 1
 from scipy import integrate
-y, err = integrate.quad(landau.get_landau_pdf, 0, 10000, args=(mu, eta))
+y, err = integrate.quad(pylandau.get_landau_pdf, 0, 10000, args=(mu, eta))
 print 'Integral of Landau PDF:', y
-y, err = integrate.quad(landau.get_gauss_pdf, 0, 10000, args=(mu, sigma))
+y, err = integrate.quad(pylandau.get_gauss_pdf, 0, 10000, args=(mu, sigma))
 print 'Integral of Gauss PDF:', y
-y, err = integrate.quad(landau.get_langau_pdf, -10000, 10000, args=(mu, eta, sigma))
+y, err = integrate.quad(pylandau.get_langau_pdf, -10000, 10000, args=(mu, eta, sigma))
 print 'Integral of Landau + Gauss (Langau) PDF:', y
