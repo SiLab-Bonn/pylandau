46fb38dbcbf0b2891cd9a005856b31693c59606c
DavidLP <david-leon.pohl@rub.de>
2016-09-02 17:04:09 +0200

    ENH: show the new API with langau scaling

--- examples/landau_gauss.py
+++ examples/landau_gauss.py
@@ -9,9 +9,14 @@ mpv, eta, sigma, A = 10, 1, 3, 1
 x = np.arange(0, 50, 0.01)
 y = pylandau.landau(x, mpv=mpv, eta=eta, A=A)
 y_gconv = gaussian_filter1d(y, sigma=sigma / 0.01)
-y_gconv_2 = pylandau.langau(x, mpv, eta, sigma, A)
+# Scaled means that the resulting Landau*Gauss function maximum is A at mpv
+# Otherwise the underlying Landau function before convolution has the 
+# function maximum A at mpv
+y_gconv_2 = pylandau.langau(x, mpv, eta, sigma, A, scale_langau=False)
+y_gconv_3 = pylandau.langau(x, mpv, eta, sigma, A, scale_langau=True)
 plt.plot(x, y, label='Landau')
 plt.plot(x, y_gconv_2, label='Langau')
 plt.plot(x, y_gconv, '--', label='Langau Scipy')
+plt.plot(x, y_gconv_3, label='Langau scaled')
 plt.legend(loc=0)
 plt.show()
