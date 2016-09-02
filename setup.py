0fa311fe1a21e80de90264b771ef451b30fd87b9
DavidLP <david-leon.pohl@rub.de>
2016-09-02 17:06:49 +0200

    PRJ: version 2.0.2 -> 2.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ else:
                                sources=['pyLandau/cpp/pylandau.cpp'],
                                language="c++")]
 
-version = '2.0.2'
+version = '2.1.0'
 author = 'David-Leon Pohl'
 author_email = 'pohl@physik.uni-bonn.de'
 
