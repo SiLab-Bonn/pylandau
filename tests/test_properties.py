ff8b270c9a386dc73c15e64cc7e1ad1425cd4501
DavidLP <pohl@physik.uni-bonn.de>
2017-12-07 10:10:41 +0100

    ENH: add test for mpv shift

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -2,9 +2,15 @@
 '''
 import unittest
 
+import numpy as np
+from hypothesis import given
+import hypothesis.strategies as st
+
 from scipy.integrate import quad as integrate
+from scipy.optimize import fmin
 
 import pylandau
+from tests import constrains
 
 
 class Test(unittest.TestCase):
@@ -27,6 +33,26 @@ class Test(unittest.TestCase):
                               0, 10000, args=(10, 3))
         self.assertAlmostEqual(result, 1, delta=1e-3)
 
+    @given(st.floats(constrains.LANDAU_PDF_MIN_MU,
+                     constrains.LANDAU_PDF_MAX_MU,
+                     allow_nan=False,
+                     allow_infinity=False))
+    def test_landau_pdf_mu(self, mu):
+        ''' Check that Landau pdf mu position '''
+        # For the given parameters (mu=mu, eta=1) MPV is expected
+        # at x = mu-0.22278
+        # See also: https://root.cern.ch/root/html524/TMath.html#TMath:Landau
+
+        # x0 > 1 otherwise scipy.optimize.fmin terminates too early
+        if abs(mu) > 1.:
+            x0 = mu
+        else:
+            x0 = 1. * np.sign(mu)
+
+        mpv = fmin(lambda x: -pylandau.get_landau_pdf(x, mu=mu, eta=1.),
+                   x0=x0, full_output=True, disp=False,
+                   xtol=0.000001, ftol=0.000001)[0][0]
+        self.assertAlmostEqual(mpv, mu - 0.22278, delta=1e9)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(Test)
