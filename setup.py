b8793c45c9275812441fd995abbd068250042c77
Christian Bespin <bespin@physik.uni-bonn.de>
2021-12-06 17:39:49 +0100

    PRJ: Use pyproject.toml

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import builtins
 from setuptools import setup, find_packages, Extension  # This setup relies on setuptools since distutils is insufficient and badly hacked code
 from setuptools.command.build_ext import build_ext as _build_ext
 
+
 class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
@@ -26,27 +27,11 @@ else:
                                sources=['pyLandau/cpp/pylandau.cpp'],
                                language="c++")]
 
-version = '2.1.2'
-author = 'David-Leon Pohl, Christian Bespin'
-author_email = 'pohl@physik.uni-bonn.de, bespin@physik.uni-bonn.de'
-
 install_requires = ['cython', 'numpy']
-setup_requires = ['cython', 'numpy']
 
 setup(
-    name='pylandau',
-    version=version,
-    description='A Landau PDF definition to be used in Python.',
-    url='https://github.com/SiLab-Bonn/pyLandau',
-    license='GNU LESSER GENERAL PUBLIC LICENSE Version 2.1',
-    long_description='The Landau propability density function is defined in C++ and made available to python via cython. Also a fast Gaus+Landau convolution is available. The interface accepts numpy arrays.',
-    author=author,
-    maintainer=author,
-    author_email=author_email,
-    maintainer_email=author_email,
     cmdclass={'build_ext': build_ext},
     install_requires=install_requires,
-    setup_requires=setup_requires,
     packages=find_packages(),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
     package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
