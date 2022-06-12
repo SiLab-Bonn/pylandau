7d0fcc9f1eaf32dbbce6d91301ace5003d77d1de
leloup314 <wolf@physik.uni-bonn.de>
2022-06-12 19:27:25 +0200

    ENH: adapt setup.py, installation working via setup.py execution

--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
-import builtins
-from setuptools import setup, find_packages, Extension  # This setup relies on setuptools since distutils is insufficient and badly hacked code
-from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
 from pylandau.pylandau_src import pylandau_numba_ext
 
 
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        builtins.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
+install_requires = ['numba>=0.53', 'numpy>=1.21', 'scipy>=1.6']
 
-install_requires = ['numba>=0.53', 'numpy>=1.21']
 
-setup(
-    cmdclass={'build_ext': build_ext},
-    install_requires=install_requires,
-    packages=find_packages(),
-    include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
-    package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
-    ext_modules=[pylandau_numba_ext.distutils_extension()],
-    keywords=['Landau', 'Langau', 'PDF'],
-    platforms='any'
+if __name__ == '__main__':
+    setup(
+        install_requires=install_requires,
+        packages=find_packages(),
+        include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
+        package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
+        ext_modules=[pylandau_numba_ext.distutils_extension()],
+        keywords=['Landau', 'Langau', 'PDF'],
+        platforms='any'
 )
