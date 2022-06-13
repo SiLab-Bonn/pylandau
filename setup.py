c9ac77bece0de0b807289838be314c63df064222
leloup314 <wolf@physik.uni-bonn.de>
2022-06-13 16:25:22 +0200

    ENH: make pip install work

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+import os, sys
 from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
+
+if os.getcwd() not in sys.path: # Needed for install instead of develop
+    sys.path.append(os.getcwd())
+
 from src.pylandau_src import pylandau_numba_ext
 
-install_requires = ['numba>=0.53', 'numpy>=1.21', 'scipy>=1.6']
+install_requires = ['numba>=0.53', 'numpy>=1.21', 'scipy>=1.7']
 
 
 if __name__ == '__main__':
