efb96ff7837abe9f1f0a299545abc3543ce8ed52
Christian Bespin <bespin@physik.uni-bonn.de>
2026-04-07 15:06:16 +0200

    prj: fix package includes

--- setup.py
+++ setup.py
@@ -10,8 +10,5 @@ from src.pylandau_src import pylandau_numba_ext
 
 if __name__ == '__main__':
     setup(
-        include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
-        package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
         ext_modules=[pylandau_numba_ext.distutils_extension()],
-        platforms='any'
 )
